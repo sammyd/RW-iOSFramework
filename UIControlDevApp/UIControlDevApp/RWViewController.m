19504256e9219183559e4c790f1b32cf16a7e0bf
Sam Davies <sdavies@shinobicontrols.com>
2014-01-21 22:54:33 +0000

    Create a dev app for the UI control framework
    This will directly reference the framework project

---
+++ UIControlDevApp/UIControlDevApp/RWViewController.m
@@ -0,0 +1,29 @@
+//
+//  RWViewController.m
+//  UIControlDevApp
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import "RWViewController.h"
+
+@interface RWViewController ()
+
+@end
+
+@implementation RWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
