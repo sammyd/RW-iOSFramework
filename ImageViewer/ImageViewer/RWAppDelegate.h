db47c14410461caaf01fe4c91c14126caf55c7f4
Sam Davies <sdavies@shinobicontrols.com>
2014-01-21 22:48:24 +0000

    Add a sample project which will use the framework
    This one will actually require the framework itself.

---
+++ ImageViewer/ImageViewer/RWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWAppDelegate.h
+//  ImageViewer
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageViewer/ImageViewer/RWViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWViewController.h
+//  ImageViewer
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWViewController : UIViewController
+
+@end
