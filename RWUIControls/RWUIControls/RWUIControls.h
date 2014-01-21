41e52db22200a916fc7914a68a07ad5582b5db7a
Sam Davies <sdavies@shinobicontrols.com>
2014-01-21 22:18:03 +0000

    Create the framework project
    It’s called RWUIControls. It will contain RWKnobControl, and an extra
    very simple control.

---
+++ RWUIControls/RWUIControls/RWUIControls.h
@@ -0,0 +1,13 @@
+//
+//  RWUIControls.h
+//  RWUIControls
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWUIControls : NSObject
+
+@end
---
+++ RWUIControls/RWUIControls/RWUIControls.m
@@ -0,0 +1,13 @@
+//
+//  RWUIControls.m
+//  RWUIControls
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import "RWUIControls.h"
+
+@implementation RWUIControls
+
+@end
