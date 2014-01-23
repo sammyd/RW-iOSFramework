cb20ccc55c987a61b34d0e5b656fccf8bfe1983a
Sam Davies <sdavies@shinobicontrols.com>
2014-01-23 22:02:53 +0000

    Imports the framework to the ImageViewer project

---
+++ ImageViewer/RWUIControls.framework/Versions/A/Headers/RWUIControls.h
@@ -0,0 +1,13 @@
+//
+//  RWUIControls.h
+//  RWUIControls
+//
+//  Created by Sam Davies on 21/01/2014.
+//  Copyright (c) 2014 RayWenderlich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// KnobControl
+#import <RWUIControls/RWKnobControl.h>
