9cb0cfb085cdc8363670108664a226d0e1e92783
Sam Davies <sdavies@shinobicontrols.com>
2014-01-23 22:43:04 +0000

    Creates very simple demo app which uses the imported framework
    This shows an image which rotates as the knob control is changed.

--- ImageViewer/ImageViewer/RWViewController.m
+++ ImageViewer/ImageViewer/RWViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "RWViewController.h"
+#import <RWUIControls/RWUIControls.h>
 
 @interface RWViewController ()
+    
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) RWKnobControl *rotationKnob;
 
 @end
 
@@ -18,8 +22,42 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect frame = self.view.bounds;
+    frame.size.height *= 2/3.0;
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectInset(frame, 0, 20)];
+    self.imageView.image = [UIImage imageNamed:@"sampleImage.jpg"];
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:self.imageView];
+    
+    frame.origin.y += frame.size.height;
+    frame.size.height /= 2;
+    frame.size.width  = frame.size.height;
+    
+    self.rotationKnob = [[RWKnobControl alloc] initWithFrame:CGRectInset(frame, 10, 10)];
+    
+    CGPoint center = self.rotationKnob.center;
+    center.x = CGRectGetMidX(self.view.bounds);
+    self.rotationKnob.center = center;
+    
+    self.rotationKnob.minimumValue = -M_PI_4;
+    self.rotationKnob.maximumValue = M_PI_4;
+    [self.rotationKnob addTarget:self
+                          action:@selector(rotationAngleChanged:)
+                forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.rotationKnob];
 }
 
+- (void)rotationAngleChanged:(id)sender
+{
+    self.imageView.transform = CGAffineTransformMakeRotation(self.rotationKnob.value);
+}
+    
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+    
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
